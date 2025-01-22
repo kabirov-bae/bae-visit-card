b8c4f7387472f6086b12063da35c734b46706da8
ewewezze <talkingtom32203@mail.ru>
2025-01-22 18:15:53 +0300

    - headings interfaced

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="container">
-    <div class="main"></div>
+    <div class="main">
+      <Heading tag-name="h1">Welcome to our website!</Heading>
+    </div>
   </div>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts" setup>
+import { Heading } from "@/shared/ui/Heading";
+</script>
 
 <style scoped></style>
---
+++ src/shared/ui/Heading/ui.vue
@@ -0,0 +1,39 @@
+<template>
+  <component :is="props.tagName" :class="['heading', bold ? 'bold' : '']">
+  </component>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  tagName: "h1" | "h2" | "h3" | "h4" | "h5";
+  bold?: boolean;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style scoped>
+h1 {
+  font-size: 64px;
+}
+
+h2 {
+  font-size: 48px;
+}
+
+h3 {
+  font-size: 36px;
+}
+
+h4 {
+  font-size: 24px;
+}
+
+h5 {
+  font-size: 18px;
+}
+
+.bold {
+  font-weight: bold;
+}
+</style>
