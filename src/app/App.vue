d2ad08b7c2fd4a9fb7cdac6dc15eb58ca2131f98
ewewezze <talkingtom32203@mail.ru>
2025-01-30 18:15:15 +0300

    - divider

--- src/app/App.vue
+++ src/app/App.vue
@@ -2,14 +2,18 @@
   <Header />
   <Content>
     <Intro />
+    <Divider />
     <Work />
+    <Divider />
     <Portfolio />
+    <Divider />
   </Content>
   <Footer />
 </template>
 
 <script lang="ts" setup>
 import { Content } from "@/shared/ui/Content";
+import { Divider } from "@/shared/ui/Divider";
 import { Header } from "@/widgets/Header";
 import { Intro } from "@/widgets/Intro";
 import { Work } from "@/widgets/Work";
---
+++ src/shared/ui/Divider/ui.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="divider"></div>
+</template>
+
+<style scoped>
+.divider {
+  width: 100%;
+  height: 1px;
+  background-color: var(--main-white);
+  opacity: 0.2
+}
+</style>
