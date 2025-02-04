43c245c9c8afeb341ae1efd65218845b73f99e61
ewewezze <talkingtom32203@mail.ru>
2025-02-04 17:55:31 +0300

    - skill

---
+++ src/widgets/Skills/ui.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Container } from "@/shared/ui/Container";
+import { Heading } from "@/shared/ui/Heading";
+</script>
+
+<template>
+  <section class="skill" id="skill">
+    <Container>
+      <Heading tag-name="h2" class="title">Skills</Heading>
+      <div class="skill__inner"></div>
+    </Container>
+  </section>
+</template>
+
+<style scoped></style>
